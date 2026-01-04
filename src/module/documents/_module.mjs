d9cec262b0aec6817e20ae6b3a2561c0715849aa
Aqui <andru.quiroga.18@cnu.edu>
2026-01-03 23:32:45 -0500

    Remove export for RogueTraderCharacter from module

--- src/module/documents/_module.mjs
+++ src/module/documents/_module.mjs
@@ -2,4 +2,3 @@ export { RogueTraderBaseActor } from "./base-actor.mjs";
 export { RogueTraderAcolyte } from "./acolyte.mjs";
 export { RogueTraderNPC } from "./npc.mjs";
 export { RogueTraderVehicle } from "./vehicle.mjs";
-export { RogueTraderCharacter } from "./rogue-trader-character.mjs";
