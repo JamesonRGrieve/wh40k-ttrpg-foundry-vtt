59af100373e1fdd9b41a13f24fa8d66da2de5bf7
Jameson <jamesonrgrieve@gmail.com>
2026-04-30 10:50:49 -0600

    Revert "fix(types): auto-fix 0 TSC errors in src/module/rules/aim.ts"
    This reverts commit aa8613318251aae1bd7aeed9f2cd88692f871f0a.

--- src/module/rules/aim.ts
+++ src/module/rules/aim.ts
@@ -1,7 +1,7 @@
 /**
  * @param rollData {RollData}
  */
-export function calculateAimBonus(rollData: RollData) {
+export function calculateAimBonus(rollData) {
     const _item = rollData.weapon ?? rollData.power;
     void _item;
 }
