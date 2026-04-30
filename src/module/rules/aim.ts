a9c336712dba1e0f4d9d73dcbd8e7655d791117d
Jameson <jamesonrgrieve@gmail.com>
2026-04-30 11:19:01 -0600

    fix(types): auto-fix 1 TSC errors in src/module/rules/aim.ts

--- src/module/rules/aim.ts
+++ src/module/rules/aim.ts
@@ -1,7 +1,7 @@
 /**
  * @param rollData {RollData}
  */
-export function calculateAimBonus(rollData) {
+export function calculateAimBonus(rollData: Record<string, unknown>) {
     const _item = rollData.weapon ?? rollData.power;
     void _item;
 }
